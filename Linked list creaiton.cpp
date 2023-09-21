5a0d1810b8e604725b7eb4b2d4bf014f8154a432
Tanvir4n <114309937+Tanvir4n@users.noreply.github.com>
2023-09-21 19:49:15 +0600

    Create Linked list creaiton.cpp

---
+++ Linked list creaiton.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+#define resetP ptr = start;
+
+typedef struct node{
+    int data;
+    struct node *next;
+}node;
+
+int main(){
+
+    struct node *node1, *node2, *node3, *start;
+
+    node1 = (struct node *)malloc(sizeof(struct node));
+    node2 = (struct node *)malloc(sizeof(struct node));
+    node3 = (struct node *)malloc(sizeof(struct node));
+
+    node1->data = 1;
+    node2->data = 2;
+    node3->data = 3;
+
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = NULL;
+    start = node1;
+
+    node *ptr;
+    //struct node *ptr = start;
+    resetP;
+
+    while(ptr != NULL){
+        cout<<ptr->data<<endl;
+        //printf("%d \n", ptr->data);
+        ptr = ptr->next;
+    }
+
+    return 0;
+}
