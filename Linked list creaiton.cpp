26c8d9518c9618dbe78f1c41ac6f7c078fc48feb
Tanvir4n <114309937+Tanvir4n@users.noreply.github.com>
2023-09-21 19:55:47 +0600

    Update Linked list creaiton.cpp

--- Linked list creaiton.cpp
+++ Linked list creaiton.cpp
@@ -15,14 +15,17 @@ int main(){
 
     struct node *node1, *node2, *node3, *start;
 
+    // Memory allocation
     node1 = (struct node *)malloc(sizeof(struct node));
     node2 = (struct node *)malloc(sizeof(struct node));
     node3 = (struct node *)malloc(sizeof(struct node));
 
+    // Data storing
     node1->data = 1;
     node2->data = 2;
     node3->data = 3;
 
+    // Link nodes
     node1->next = node2;
     node2->next = node3;
     node3->next = NULL;
@@ -31,7 +34,7 @@ int main(){
     node *ptr;
     //struct node *ptr = start;
     resetP;
-
+    // Print the linked list
     while(ptr != NULL){
         cout<<ptr->data<<endl;
         //printf("%d \n", ptr->data);
